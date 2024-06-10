07e62b613d38030609edb9ef65b0307b365056bb
Pooja Patel <122346906+PoojaPatel6@users.noreply.github.com>
2024-06-10 16:08:08 +0530

    Add files via upload
    Revision of java

---
+++ 10_June_2024/Revision_JavaProgram/Variables.java
@@ -0,0 +1,21 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+//		int x;//Declaration
+//		x=123;//assignment
+		int x =123;//Initilization
+		float y = 3.17f;
+		boolean z = false;
+		char symbol = '@';
+		String name = "Pooja Patel";
+		System.out.println("My number is :"+x);
+		System.out.println("My float number is:"+y);
+		System.out.println("My boolean number is:"+z);
+		System.out.println("My char number is:"+symbol);
+		System.out.println("My name is:"+name);
+
+	}
+
+}
