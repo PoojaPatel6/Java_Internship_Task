084eaeed30e1c4218ae336268f6f101ed9c6f3de
Pooja Patel <122346906+PoojaPatel6@users.noreply.github.com>
2024-06-24 10:09:11 +0530

    Update MultidimentionArray.java

--- 21_June_2024/MultidimentionArray.java
+++ 21_June_2024/MultidimentionArray.java
@@ -26,6 +26,21 @@ Scanner in = new Scanner(System.in);
                 arr[row][col] = in.nextInt();
             }
         }
+
+              for(int row = 0; row< arr.length; row++){
+ //            for(int col = 0; col< arr[row].length; col++){
+ //                System.out.println(arr[row][col] + " ");
+//            }
+//            System.out.println();
+//        }
+
+
+        for (int row = 0; row < arr.length; row++){
+            System.out.println(Arrays.toString(arr[row]));
+        }
+        for(int[] a : arr){
+            System.out.println(Arrays.toString(a));
+        }
     }
 
 }
