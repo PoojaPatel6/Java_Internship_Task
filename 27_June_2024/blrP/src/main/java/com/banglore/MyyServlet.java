2d0a079b338358c33ccb33cb219d653ceb88c30c
Pooja Patel <122346906+PoojaPatel6@users.noreply.github.com>
2024-06-27 17:06:20 +0530

    Add files via upload
    servlet

---
+++ 27_June_2024/blrP/src/main/java/com/banglore/MyyServlet.java
@@ -0,0 +1,20 @@
+package com.banglore;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+
+public class MyyServlet  extends HttpServlet{
+
+	public void service(HttpServletRequest req, HttpServletResponse res) {
+		
+		String a = req.getParameter("fname");
+		String b = req.getParameter("pass");
+	    System.out.println(a + "  " + b);
+		
+	    
+	    
+	}
+}
---
+++ 27_June_2024/blrP/src/main/java/com/banglore/calcC.java
@@ -0,0 +1,18 @@
+package com.banglore;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+
+public class calcC extends HttpServlet{
+	
+	public void service(HttpServletRequest req, HttpServletResponse res) {
+		int x = Integer.parseInt(req.getParameter("n1"));
+		int y = Integer.parseInt(req.getParameter("n2"));
+		int z = x * y;
+		System.out.println(z);
+	}
+
+}
