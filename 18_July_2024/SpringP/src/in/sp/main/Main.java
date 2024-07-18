1b75fbb844d583df8934f53afbf0c02759815ce7
Pooja Patel <122346906+PoojaPatel6@users.noreply.github.com>
2024-07-18 13:37:52 +0530

    Add files via upload
    Spring Project in eclipse

---
+++ 18_July_2024/SpringP/src/in/sp/main/Main.java
@@ -0,0 +1,20 @@
+package in.sp.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import in.sp.beans.StudentC;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ApplicationContext cxt = new ClassPathXmlApplicationContext("in/sp/config/applicationContext.xml");
+		StudentC s1 = (StudentC) cxt.getBean("patna1");
+		s1.show();
+		
+		
+		StudentC s2 = (StudentC) cxt.getBean("ddn");
+		s2.show();
+	}
+
+}
