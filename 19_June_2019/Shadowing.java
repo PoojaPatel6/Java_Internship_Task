c4b1b66756f78bc7cf31b39134b40347b3f23e24
Pooja Patel <122346906+PoojaPatel6@users.noreply.github.com>
2024-09-04 16:21:47 +0530

    Update Shadowing.java

--- 19_June_2019/Shadowing.java
+++ 19_June_2019/Shadowing.java
@@ -1,10 +1,10 @@
 package com.pooja;
 public class Shadowing {
 
-    static int x = 90; // it will be shadow
+    static int x = 80; // it will be shadow
     public static void main(String[] args) {
 
-        System.out.println(x); // 90
+        System.out.println(x); // 80
         int x;
         x = 40;   // class variable at line 4 is shadowed by this
         System.out.println(x);  //40
