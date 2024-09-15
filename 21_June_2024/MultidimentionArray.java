cf36632047c7f572c2e55174166b13e12fff12ac
Pooja Patel <122346906+PoojaPatel6@users.noreply.github.com>
2024-09-15 22:00:04 +0530

    Update MultidimentionArray.java

--- 21_June_2024/MultidimentionArray.java
+++ 21_June_2024/MultidimentionArray.java
@@ -20,7 +20,7 @@ Scanner in = new Scanner(System.in);
 
 
         for(int row = 0; row < arr.length; row++){
-            // for each col in every row
+            // for each col in every row///
 
             for(int col = 0; col < arr[row].length; col++){
                 arr[row][col] = in.nextInt();
