1f97089d9c7b5e7a147bb7abf7c5965572d75a66
Pooja Patel <122346906+PoojaPatel6@users.noreply.github.com>
2024-09-22 21:12:28 +0530

    Update Compiling.java

--- 31_may_2024/Compiling.java
+++ 31_may_2024/Compiling.java
@@ -5,7 +5,7 @@ public class Compiling {
     System.out.println("Java classes have a 'main' method.");
     System.out.println("Java statements end with a semicolon.");
 
-    System.out.println("Programming is... fun!");
+    System.out.println("Programming is fun!");
     
   }
 }
