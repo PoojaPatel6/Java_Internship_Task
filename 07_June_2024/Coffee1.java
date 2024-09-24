273d77b3178727c25ce4a37e4cbd60ddb7154da1
Pooja Patel <122346906+PoojaPatel6@users.noreply.github.com>
2024-09-24 17:02:38 +0530

    Update Coffee1.java

--- 07_June_2024/Coffee1.java
+++ 07_June_2024/Coffee1.java
@@ -6,7 +6,7 @@ class Coffee1 {
       
       for (int cupsOfCoffee = 1; cupsOfCoffee <= 100; cupsOfCoffee++) {
         
-        System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+        System.out.println("Fry Drinks cup of coffee #" + cupsOfCoffee);
         
       }
       
