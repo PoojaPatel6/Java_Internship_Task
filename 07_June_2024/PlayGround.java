bf0aee0944dc6fb20e78961d5ec7ee9e2b472b15
Pooja Patel <122346906+PoojaPatel6@users.noreply.github.com>
2024-09-26 09:29:25 +0530

    Update PlayGround.java

--- 07_June_2024/PlayGround.java
+++ 07_June_2024/PlayGround.java
@@ -8,7 +8,7 @@ class PlayGround {
 	public static void main(String[] args) {
   
     for (int i = 0; i < 28; i++) {
-  		System.out.println("Congrats on finishing Java loops!");
+  		System.out.println("Congrats on Finishing Java loops!");
 	  }
   }
 
